69b3065c0003636b28dbef20b5090c42da62b92f
ElectricCrimson <synthetic0aesthetic@gmail.com>
2025-01-18 13:56:32 +0300

    feat: solve read file task

---
+++ 01-read-file/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+const pathJoin = path.join(__dirname, 'text.txt');
+const stream = fs.createReadStream(pathJoin, 'utf-8');
+
+stream.on('error', (error) => {
+  console.error('Error!', error);
+});
+
+stream.on('data', (data) => {
+  console.log(data);
+});
